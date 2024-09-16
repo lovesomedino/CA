4e3e930af0967781ba552507444929ef6935c77d
lovesomedino <lovesomedino@gmail.com>
2024-09-16 17:29:57 +0900

    backend 세팅: 파일 업로드

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Files(models.Model):
+    file = models.FileField(null=True)
+
+    def __str__(self):
+        return f'{self.file}' 
