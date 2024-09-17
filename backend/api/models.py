7997cfc9abfb3609ef7f3d25cf9cbf5ed82035a8
lovesomedino <lovesomedino@gmail.com>
2024-09-17 17:31:31 +0900

    파일 삭제 구현(실제 파일도 삭제)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.conf import settings
+import os
 
 class Files(models.Model):
     file = models.FileField(null=True)
 
     def __str__(self):
         return f'{self.file}' 
+
+    def delete(self, *args, **kwargs):
+        super(Files, self).delete(*args, **kwargs)
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.file.path))   
