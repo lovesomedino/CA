a4791d062500da1d352485087525c35ac787d4bd
lovesomedino <lovesomedino@gmail.com>
2024-10-15 22:01:00 +0900

    chart.js 라이브러리 활용하여 chart 화면 구현(샘플 데이터로 추후 변경)

--- frontend/src/components/ChartView.js
+++ frontend/src/components/ChartView.js
@@ -1,13 +1,53 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import BarChart from './charts/BarChart'
+import PieChart from './charts/PieChart'
+import LineChart from './charts/LineChart'
 
 function ChartView() {
     const location = useLocation();
     const fileId = location.state.id;
 
     return (
-        <div>
-            <div style={{marginLeft: "240px", height: "100vh"}}>Chart</div>
+        <div style={{marginLeft: "240px", height: "100vh"}}>
+            <div class="container-fluid">
+                 <div class="row">
+                    <div class="col-xl-8 col-lg-7">
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="chart-bar">
+                                    <BarChart />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="chart-area">
+                                    <LineChart />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Pie Chart</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="chart-pie pt-4">
+                                    <PieChart />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
